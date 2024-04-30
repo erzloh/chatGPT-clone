e0e1e0e7c2740c2789cfa8543e6f04739f7b4613
Eric Holzer <eric.holzer.lsn@gmail.com>
2024-04-30 13:48:23 +0200

    update fetch url

--- client/src/components/ChatboxInput.jsx
+++ client/src/components/ChatboxInput.jsx
@@ -30,7 +30,7 @@ const ChatboxInput = ({ messages, setMessages, isGptAnswering, setIsGptAnswering
 	async function addApiResponseToMessagesState(messagesArray) {
 		try {
 			setIsGptAnswering(true)
-			const response = await fetch('https://mychatgptserver-g4ljpds3t-erzlohs-projects.vercel.app/', {
+			const response = await fetch('https://mychatgptserver-gz824j02i-erzlohs-projects.vercel.app', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
